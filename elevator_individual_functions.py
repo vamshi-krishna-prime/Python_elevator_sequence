8bf645199c5ff6b6897f813a71df4f9f0f97719c
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-05-29 23:44:49 +0530

    Update elevator_individual_functions.py

--- elevator_individual_functions.py
+++ elevator_individual_functions.py
@@ -1,31 +1,45 @@
 import time
 
-def Intro():
+def print_delay(message):
+    print(message)
+    time.sleep(1)
+
+def intro():
     print_delay("You have just arrived at your new job!")
     print_delay("You are in the elevator.")
 
-def print_delay(string):
-    print(string)
-    time.sleep(1)
+def floor(num, department):
+    print_delay("You push the button for the " + num + "floor.")
+    print_dealy("After a few movements, you find yourself in the " + department)
 
-def floor(number, option):
-    print_delay("You push the button for the " + number + " floor.")
-    print_delay("After a few movements, you find yourself in the " + option + ".")
-    print_delay("Where would you like to go next?")
+def first_floor():
+    global first_count
+    global id_card
+    first_count += 1
+    print_delay(first_count)
+    print_delay(id_card)
+    if first_count == 1 and id_card == "not received":
+        print_delay("The clerk greets you and gives you your ID card.")
+        id_card = "received"
+    elif first_count > 1 and id_card == "received":
+        print_delay("The clerk greets you, "
+                    "but shea has already given you your ID card, "
+                    "so there is nothing more to do here now.")
 
 def choose_floor():
-    print_delay("Please enter the number for the floor"
-                " you would like to visit:")
-    choice=input("1.Lobby\n"
-                "2.Human resources\n"
-                "3.Engineering department\n")
+    print_delay("Plaese enter a number for the floor you would like to visit")
+    choice= input("1. Lobby \n"
+                  "2. Human resources \n"
+                  "3. Engineering department \n")
     if choice == "1":
-        floor("first", "lobby")
-    elif choice == "2":
-        floor("second","human resources")
-    elif choice == "3":
-        floor("third","engineering department")
+        first_floor()
     choose_floor()
 
-Intro()
+
+
+first_count = 0
+second_count = 0
+third_count = 0
+id_card = "not received"
+intro()
 choose_floor()
