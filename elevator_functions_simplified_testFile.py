4ca4b9437f2247e8e057a21149f72f792372c5d8
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-05-29 23:49:52 +0530

    Update elevator_functions_simplified_testFile.py

--- elevator_functions_simplified_testFile.py
+++ elevator_functions_simplified_testFile.py
@@ -4,15 +4,32 @@ def print_delay(message):
     print(message)
     time.sleep(1)
 
-complex= [["a","b","c"],
+list1= [["a","b","c"],
        ["d","e","f"],
        "",
-       "1","2","3"]
+       ""]
 
-def floor_action(list):
-    for index in range(len(list)):
-        for alpha in list[index]:
-            print_delay(alpha)
+list2= [["a"],
+       ["b"],
+       ["c"],
+       ["d"]]
 
-floor_action(complex)
+list3= [["e"],
+       ["f"],
+       ["g"],
+       ["h"]]
 
+
+def floor_action(list, extra):
+    test= int(input("enter a number:"))
+    if test == 1:
+        for index in list[0]:
+            print_delay(index)
+    elif test == 2:
+        print_delay(list[1])
+        if extra == "no":
+            print_delay(list[2])
+        elif extra == "yes":
+            print_delay(list[3])
+
+floor_action(list1, "no")
