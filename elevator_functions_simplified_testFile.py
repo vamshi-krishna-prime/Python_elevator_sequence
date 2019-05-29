6484dbb3acc164dd6c2763877bda2a5d095fa477
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-05-29 23:49:27 +0530

    Update elevator_functions_simplified_testFile.py

--- elevator_functions_simplified_testFile.py
+++ elevator_functions_simplified_testFile.py
@@ -4,10 +4,15 @@ def print_delay(message):
     print(message)
     time.sleep(1)
 
-complex= ["a","b","","","c"]
+complex= [["a","b","c"],
+       ["d","e","f"],
+       "",
+       "1","2","3"]
 
 def floor_action(list):
-    for alpha in list:
-        print_delay(alpha)
+    for index in range(len(list)):
+        for alpha in list[index]:
+            print_delay(alpha)
 
 floor_action(complex)
+
